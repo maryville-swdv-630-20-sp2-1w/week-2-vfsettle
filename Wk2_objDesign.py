f504b4912d5bfab9327b7250a3066cfcf0e8f315
vfsettle <56850679+vfsettle@users.noreply.github.com>
2020-03-19 20:14:27 -0700

    Update Wk2_objDesign.py
    Final commit. Added class attributes for account holders name and account information.

--- Wk2_objDesign.py
+++ Wk2_objDesign.py
@@ -1,31 +1,62 @@
 class CheckingAccount:
-        def __init__(self):
-            self.__balance = 0
-            print ("This is a deposit and withdrawal application. \n Your starting balance is", self.__balance)
+        def __init__(self, fName, lName, acctNum, balance =0):
+            self.fName = fName
+            self.lName = lName
+            self.acctNum = acctNum
+            self.__balance = balance
+            self.history = []
+            print ("Welcome", self.fName, self.lName,", this is a deposit and withdrawal application.\n")
         
+        def get_fName (self):
+            return self.fName
+        
+        def get_lName (self):
+            return self.lName
+        
+        def get_acctNum (self):
+            return self.acctNum
+        
+        def get_balance (self):
+            return self.balance
+        
+        def get_last(self):
+            if len(self.history) < 1:
+                return None
+            else:
+                print ("\n Your most recent transaction was")
+                return self.history.pop()
+
         def viewBal (self):
-            print("\n Your current balance is", self.__balance)
+            print("\n Your current balance is", self.__balance, "\n")
         
-        def deposit (self):
-            amount = float(input("Enter Deposit Amount: "))
+        def deposit (self, amount):
             self.__balance += amount
-            print ("\n", amount , "Deposited for New Balance of", self.__balance)
-            
-        def withdraw (self):
-            amount = float(input("Enter Withdrawal Amount: "))
+            self.history.append(+amount)
+            print ("\n", amount , "Deposited for New Balance of", self.__balance, "\n")
+            return amount
+        
+        def withdraw (self, amount):
             if self.__balance >= amount:
                 self.__balance -= amount
-                print (amount, "Withdrawn for New Balance of", self.__balance)
+                self.history.append(-amount)
+                print (amount, "Withdrawn for New Balance of", self.__balance, "\n")
+                return amount
             else:
-                print ("Unable to withdraw", amount, "from balance of", self.__balance)
+                print("Unable to withdraw", amount, "from balance of", self.__balance, "\n")
 
 
 def main():
-    sample = CheckingAccount()
-    sample.deposit()
-    sample.deposit()
-    sample.withdraw()
+    sample = CheckingAccount('Maven', 'Miller', 987654321)
+    print (sample.get_fName())
+    print (sample.get_lName())
+    print (sample.get_acctNum())
+    sample.deposit(600)
+    sample.deposit(400)
+    sample.withdraw(1500)
+    sample.withdraw(200)
+    print (sample.get_last())
+    sample.deposit (550)
     sample.viewBal()
-    sample.withdraw()
+    print (sample.get_last())
         
 main()
